2acbd33a6d5d873485b444247f371ec93644e56b
miso062 <21600062@handong.edu>
2019-05-18 22:29:41 +0900

    miso

--- src/database/testMain.java
+++ src/database/testMain.java
@@ -10,6 +10,234 @@ public class testMain {
 static String url;
 static int curUserID;
 
+public static void showProfile(Statement stmt) {
+	ResultSet rs = null;
+	
+	String sql = "SELECT u.User_Id, u.User_name, u.Intro, u.Partener_User_id "
+			+ "FROM UserList AS u "
+			+ "WHERE u.User_Id = "+curUserID+";";
+	
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[프로필 보기]");
+		/*while(rs.next()) {
+			System.out.println(rs.getInt("User_Id")+". "+rs.getString("User_name")+". "+rs.getString("Intro")
+				+". "+countFollowedList(stmt, curUserID)+". "+countFollowingList(stmt, curUserID));
+		}*/
+		rs.next();
+		System.out.println(rs.getInt("User_Id")+". "+rs.getString("User_name")+". "+rs.getString("Intro")
+			+". "+countFollowedList(stmt, curUserID)+". "+countFollowingList(stmt, curUserID));
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+public static void editProfile(Statement stmt, String Intro){
+	
+	String sql = "UPDATE UserList SET Intro = '"+Intro+"' "
+			+ "WHERE User_Id = "+curUserID+";";
+	
+	try {
+		stmt.executeUpdate(sql);
+		//System.out.println("[프로필 수정]");
+		//showProfile(stmt);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+public static int countFollowingList(Statement stmt, int userId) {
+	ResultSet rs = null;
+	int followingCount = 0;
+	
+	String sql = "SELECT f.Followed_User_Id, u.User_name, u.Intro "
+			+ "FROM followList AS f "
+			+ "JOIN UserList AS u "
+			+ "WHERE f.Following_User_Id = "+userId+" AND f.Followed_User_ID = u.User_Id;";
+	
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		while(rs.next()) {
+			followingCount++;
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+	return followingCount;
+}
+
+public static int countFollowedList(Statement stmt, int userId) {
+	ResultSet rs = null;
+	int followedCount = 0;
+	
+	String sql = "SELECT f.Following_User_Id, u.User_name, u.Intro "
+			+ "FROM followList AS f "
+			+ "JOIN UserList AS u "
+			+ "WHERE f.Followed_User_Id = "+userId+" AND f.Following_User_ID = u.User_Id;";
+	
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		while(rs.next()) {
+			followedCount++;
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return followedCount;
+}
+
+public static void followingListPrint(Statement stmt, int userId) {
+	ResultSet rs = null;
+	
+	String sql = "SELECT f.Followed_User_Id, u.User_name, u.Intro "
+			+ "FROM followList AS f "
+			+ "JOIN UserList AS u "
+			+ "WHERE f.Following_User_Id = "+userId+" AND f.Followed_User_ID = u.User_Id;";
+	
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[팔로잉 목록]");
+		while(rs.next()) {
+			System.out.println(rs.getInt("Followed_User_Id")+". "+rs.getString("User_name")+". "+rs.getString("Intro"));
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+public static void followedListPrint(Statement stmt, int userId) {
+	ResultSet rs = null;
+	
+	String sql = "SELECT f.Following_User_Id, u.User_name, u.Intro "
+			+ "FROM followList AS f "
+			+ "JOIN UserList AS u "
+			+ "WHERE f.Followed_User_Id = "+userId+" AND f.Following_User_ID = u.User_Id;";
+	
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[팔로워 목록]");
+		while(rs.next()) {
+			System.out.println(rs.getInt("Following_User_Id")+". "+rs.getString("User_name")+". "+rs.getString("Intro"));
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+public static void myMovieEvaluating(Statement stmt) {
+	ResultSet rs = null;
+	
+	String sql = "SELECT m.movie_id, m.movie_name, em.star_point "
+			+ "FROM movie AS m "
+			+ "JOIN evaluate_movie AS em "
+			+ "WHERE em.eval_user_id = "+curUserID+" AND em.eval_movie_id=m.movie_id;";
+	   
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[평가한 영화목록]");
+		while(rs.next()) {
+			System.out.println(rs.getInt("movie_id")+". "+rs.getString("movie_name")+". "+rs.getFloat("star_point"));
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}	
+}
+
+public static void myMovieWantToWatching(Statement stmt) {
+	
+	ResultSet rs = null;
+	
+	String sql = "SELECT m.movie_id, m.movie_name "
+			+ "FROM movie AS m "
+			+ "JOIN store_movie AS sm "
+			+ "WHERE sm.kind=0 AND sm.user_id = "+curUserID+" AND sm.movie_id=m.movie_id;";
+	   
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[보고싶어요 영화목록]");
+		while(rs.next()) {
+			System.out.println(rs.getInt("movie_id")+". "+rs.getString("movie_name"));
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}	
+}
+
+public static void myMovieDontInteresting(Statement stmt) {
+	
+	ResultSet rs = null;
+	
+	String sql = "SELECT m.movie_id, m.movie_name "
+			+ "FROM movie AS m "
+			+ "JOIN store_movie AS sm "
+			+ "WHERE sm.kind=1 AND sm.user_id = "+curUserID+" AND sm.movie_id=m.movie_id;";
+	   
+	try {
+		rs = stmt.executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+  
+	try {
+		System.out.println("[관심없어요 영화목록]");
+		while(rs.next()) {
+			System.out.println(rs.getInt("movie_id")+". "+rs.getString("movie_name"));
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}	
+}
+
+public static void Delete_EvaluateMovie(Statement stmt, int movie_id) {
+	String sql;
+	
+	sql = "DELETE FROM evaluate_movie WHERE eval_user_id = "+curUserID+" "
+			+ " AND eval_movie_id = "+movie_id+";"; 
+	
+	   try {
+		stmt.executeUpdate(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+
 public static void myMovieWatching(Statement stmt) {
 	
 	ResultSet rs = null;
@@ -27,7 +255,7 @@ public static void myMovieWatching(Statement stmt) {
 	}
   
 	try {
-		System.out.println("[보는 중 영화목록]");
+		System.out.println("[蹂대뒗 以� �쁺�솕紐⑸줉]");
 		while(rs.next()) {
 			System.out.println(rs.getInt("movie_id")+". "+rs.getString("movie_name"));
 		}
@@ -62,7 +290,7 @@ public static void MyCollectionPrint(Statement stmt) {
 	String sql = "SELECT collection_id, collection_name "
 			+ "FROM collection "
 			+ "WHERE user_id = " + curUserID +";";
-	//is deleted 조건
+	//is deleted 議곌굔
 	   
 	try {
 		rs = stmt.executeQuery(sql);
@@ -72,7 +300,7 @@ public static void MyCollectionPrint(Statement stmt) {
 	}
   
 	try {
-		System.out.println("[내가 만든 컬렉션]");
+		System.out.println("[�궡媛� 留뚮뱺 而щ젆�뀡]");
 		while(rs.next()) {
 			System.out.println(rs.getInt("collection_id")+". "+rs.getString("collection_name"));
 		}
@@ -89,7 +317,7 @@ ResultSet rs = null;
 			+ "FROM collection AS c "
 			+ "JOIN collectionLike AS cl "
 			+ "WHERE cl.user_id = "+curUserID+" AND c.collection_id = cl.collection_id;";
-	//is deleted 조건
+	//is deleted 議곌굔
 	
 	try {
 		rs = stmt.executeQuery(sql);
@@ -99,7 +327,7 @@ ResultSet rs = null;
 	}
   
 	try {
-		System.out.println("[좋아요한 컬렉션]");
+		System.out.println("[醫뗭븘�슂�븳 而щ젆�뀡]");
 		while(rs.next()) {
 			System.out.println(rs.getInt("collection_id")+". "+rs.getString("collection_name"));
 		}
@@ -109,7 +337,7 @@ ResultSet rs = null;
 	}
 }
 
-//index 초기화 - 삭제한 경우에 사용 
+//index 珥덇린�솕 - �궘�젣�븳 寃쎌슦�뿉 �궗�슜 
 public static void RearrangeIndex(Statement stmt,String tableName, String indexName) {
 	String sql,sql2,sql3;
 	sql = "ALTER TABLE collection DROP COLUMN collection_id;";
@@ -138,17 +366,17 @@ public static void MyCommentPrint(Statement stmt) {
 	sql1 = "SELECT comment_id, comment_content "
 			+ "FROM collectionComments "
 			+ "WHERE user_id = " + curUserID +";";
-	//is deleted 조건
+	//is deleted 議곌굔
 	
 	sql2 = "SELECT comment_id, comment_content "
 			+ "FROM movieComments "
 			+ "WHERE user_id = " + curUserID +";";
-	//is deleted 조건
+	//is deleted 議곌굔
 	   
 	try {
 		rs1 = stmt.executeQuery(sql1);
 		
-		System.out.println("[내가 작성한 코멘트(영화)]");
+		System.out.println("[�궡媛� �옉�꽦�븳 肄붾찘�듃(�쁺�솕)]");
 		while(rs1.next()) {
 			System.out.println(rs1.getInt("comment_id")+". "+rs1.getString("comment_content"));
 		}
@@ -160,7 +388,7 @@ public static void MyCommentPrint(Statement stmt) {
 	try {
 		rs2 = stmt.executeQuery(sql2);
 		
-		System.out.println("[내가 작성한 코멘트(컬렉)]");
+		System.out.println("[�궡媛� �옉�꽦�븳 肄붾찘�듃(而щ젆)]");
 		while(rs2.next()) {
 			System.out.println(rs2.getInt("comment_id")+". "+rs2.getString("comment_content"));
 		}
@@ -179,13 +407,13 @@ public static void LikeMovieCommentPrint(Statement stmt) {
 			"	movieCommentLike inner join movie on movie.movie_id = movieCommentLike.movie_id " + 
 			"    inner join movieComments on movieCommentLike.commnet_id = movieComments.comment_id "
 			+ "WHERE movieCommentLike.user_id = " + curUserID +";";
-	//is deleted 조건
+	//is deleted 議곌굔
 
 	   
 	try {
 		rs = stmt.executeQuery(sql);
 		
-		System.out.println("[내가 좋아한 코멘트(영화)]");
+		System.out.println("[�궡媛� 醫뗭븘�븳 肄붾찘�듃(�쁺�솕)]");
 		while(rs.next()) {
 			System.out.println(rs.getInt("comment_id")+". "+rs.getString("comment_content")
 			+" - "+rs.getString("movie_name"));
@@ -206,13 +434,13 @@ public static void LikeCollectionCommentPrint(Statement stmt) {
 			"	collectionCommentLike inner join collection on collection.collection_id = collectionCommentLike.collection_id " + 
 			"    inner join collectionComments on collectionComments.comment_id = collectionCommentLike.comment_id "
 			+ "WHERE collectionCommentLike.user_id = " + curUserID +";";
-	//is deleted 조건
+	//is deleted 議곌굔
 
 	   
 	try {
 		rs = stmt.executeQuery(sql);
 		
-		System.out.println("[내가 좋아한 코멘트(컬렉션)]");
+		System.out.println("[�궡媛� 醫뗭븘�븳 肄붾찘�듃(而щ젆�뀡)]");
 		while(rs.next()) {
 			System.out.println(rs.getInt("comment_id")+". "+rs.getString("comment_content")
 			+" - "+rs.getString("collection_name"));
@@ -244,7 +472,7 @@ public static void starDistribution(Statement stmt) {
 	}
   
 	try {
-		System.out.println("[내 별점 분포]");
+		System.out.println("[�궡 蹂꾩젏 遺꾪룷]");
 		while(rs.next()) {
 			 temp = rs.getInt("star_point");
 			 
@@ -318,12 +546,12 @@ public static void myMovieinfo(Statement stmt) {
 		e.printStackTrace();
 	}
 	
-	System.out.println("[평가한 영화 수] : "+count+"회");
-	System.out.println("[영화 감상 시간] : 약 " + Math.round((float)total/60) + "시간");
+	System.out.println("[�룊媛��븳 �쁺�솕 �닔] : "+count+"�쉶");
+	System.out.println("[�쁺�솕 媛먯긽 �떆媛�] : �빟 " + Math.round((float)total/60) + "�떆媛�");
 }
 
 public static void myfavMovieActorDirector(Statement stmt) {
-	//선호 배우, 감독
+	//�꽑�샇 諛곗슦, 媛먮룆
 	ResultSet rs = null;
 	
 	List<String> actor_list = new ArrayList<>();
@@ -346,7 +574,7 @@ public static void myfavMovieActorDirector(Statement stmt) {
 	
 	try {
 		while(rs.next()) {
-			//인물 성향에 따라 분류 
+			//�씤臾� �꽦�뼢�뿉 �뵲�씪 遺꾨쪟 
 				if(rs.getInt("cast_personality") == 0){
 					actor_list.add(rs.getString("person_name"));
 				}
@@ -362,7 +590,7 @@ public static void myfavMovieActorDirector(Statement stmt) {
 	Random rand = new Random();
 	
 	if(actor_list.size()/2>0) {
-		System.out.println("[선호배우]");
+		System.out.println("[�꽑�샇諛곗슦]");
 		for(int i=0; i<actor_list.size()/2;i++) {
 			int ran = rand.nextInt(actor_list.size());
 			
@@ -375,12 +603,12 @@ public static void myfavMovieActorDirector(Statement stmt) {
 		}
 	}	
 	else {
-		System.out.println("[선호배우]");
-		System.out.println("더 많은 영화 평가가 필요합니다.");
+		System.out.println("[�꽑�샇諛곗슦]");
+		System.out.println("�뜑 留롮� �쁺�솕 �룊媛�媛� �븘�슂�빀�땲�떎.");
 	}
 	
 	if(dir_list.size()/2>0) {
-		System.out.println("[선호감독]");
+		System.out.println("[�꽑�샇媛먮룆]");
 		for(int i=0; i<dir_list.size()/2;i++) {
 			int ran = rand.nextInt(dir_list.size());
 		
@@ -393,14 +621,14 @@ public static void myfavMovieActorDirector(Statement stmt) {
 		}
 	}	
 	else {
-		System.out.println("[선호감독]");
-		System.out.println("더 많은 영화 평가가 필요합니다.");
+		System.out.println("[�꽑�샇媛먮룆]");
+		System.out.println("�뜑 留롮� �쁺�솕 �룊媛�媛� �븘�슂�빀�땲�떎.");
 	}
 	
 }
 
 public static void myfavMovieTagGenre(Statement stmt) {
-	//장르,tag
+	//�옣瑜�,tag
 	
 	ResultSet rs_genre = null;
 	ResultSet rs_tag = null;
@@ -455,7 +683,7 @@ public static void myfavMovieTagGenre(Statement stmt) {
 	Random rand = new Random();
 	
 	if(genre_list.size()/2>0) {
-		System.out.println("[선호장르]");
+		System.out.println("[�꽑�샇�옣瑜�]");
 		for(int i=0; i<genre_list.size()/2;i++) {
 			int ran = rand.nextInt(genre_list.size());
 			
@@ -468,12 +696,12 @@ public static void myfavMovieTagGenre(Statement stmt) {
 		}
 	}	
 	else {
-		System.out.println("[선호장르]");
-		System.out.println("더 많은 영화 평가가 필요합니다.");
+		System.out.println("[�꽑�샇�옣瑜�]");
+		System.out.println("�뜑 留롮� �쁺�솕 �룊媛�媛� �븘�슂�빀�땲�떎.");
 	}
 	
 	if(tag_list.size()/2>0) {
-		System.out.println("[선호 tag]");
+		System.out.println("[�꽑�샇 tag]");
 		for(int i=0; i<tag_list.size()/2;i++) {
 			int ran = rand.nextInt(tag_list.size());
 			if(!fav_tag_list.contains(tag_list.get(ran))){
@@ -485,14 +713,14 @@ public static void myfavMovieTagGenre(Statement stmt) {
 		}
 	}	
 	else {
-		System.out.println("[선호 tag]");
-		System.out.println("더 많은 영화 평가가 필요합니다.");
+		System.out.println("[�꽑�샇 tag]");
+		System.out.println("�뜑 留롮� �쁺�솕 �룊媛�媛� �븘�슂�빀�땲�떎.");
 	}
 	
 }
 
 public static void myfavMovieCountry(Statement stmt) {
-	//국가
+	//援�媛�
 	
 	ResultSet rs_country = null;
 	
@@ -519,7 +747,7 @@ public static void myfavMovieCountry(Statement stmt) {
 	Random rand = new Random();
 	
 	if(country_list.size()/2>0) {
-		System.out.println("[선호국가]");
+		System.out.println("[�꽑�샇援�媛�]");
 		for(int i=0; i<country_list.size()/2;i++) {
 			int ran = rand.nextInt(country_list.size());
 			
@@ -532,8 +760,8 @@ public static void myfavMovieCountry(Statement stmt) {
 		}
 	}	
 	else {
-		System.out.println("[선호국가]");
-		System.out.println("더 많은 영화 평가가 필요합니다.");
+		System.out.println("[�꽑�샇援�媛�]");
+		System.out.println("�뜑 留롮� �쁺�솕 �룊媛�媛� �븘�슂�빀�땲�떎.");
 	}
 	
 }
@@ -548,22 +776,22 @@ public static void main(String[] args) {
 
     ResultSet rs = null;
     
-    //현재 user 지정
+    //�쁽�옱 user 吏��젙
     curUserID = 0;
 
     try{
 
-    Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); // JDBC 드라이버 로드
+    Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); // JDBC �뱶�씪�씠踰� 濡쒕뱶
 
         conn = DriverManager.getConnection("jdbc:mysql://141.223.207.26/DBclass", "yc12312", "yc9639631!");//URL,UID,PWD        
 
         if(conn==null){
 
-            System.out.println("연결실패");
+            System.out.println("�뿰寃곗떎�뙣");
 
         }else{
 
-//            System.out.println("연결성공");
+//            System.out.println("�뿰寃곗꽦怨�");
             
             stmt = conn.createStatement();
             
@@ -608,7 +836,7 @@ public static void main(String[] args) {
 
     }finally{
 
-        try{ // 연결 해제(한정돼 있으므로)
+        try{ // �뿰寃� �빐�젣(�븳�젙�뤌 �엳�쑝誘�濡�)
 
             if(rs!=null){        rs.close();            }
 
